a3f4cb83717cd2803db791360aade76f7de06cee
Philippe Huron <36928809+philippehuron@users.noreply.github.com>
2020-10-01 13:35:29 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -9,7 +9,7 @@ from pyti.relative_strength_index import relative_strength_index as rsi
 #token = "1a7dd8a06ab9e998d650cb750f2bd039860af8c2"
 #moi
 token = 'cb0685731aac9d8a261be8c73e16d33c33069722'
-symbol = ['EUR/USD', 'GBP/USD', 'EUR/JPY', 'USD/JPY', 'CHF/JPY']
+symbols = ['EUR/USD', 'GBP/USD', 'EUR/JPY', 'USD/JPY', 'CHF/JPY']
 timeframe = "m1"
 rsi_periods = 14
 upper_rsi = 70.0
@@ -19,10 +19,10 @@ stop = -5
 limit = 10
 #############################
 
-symbolEnter = []
+openSymbols = []
 
-def enter(is_buy, d):
-    print("Open enter")
+def position(is_buy, d):
+    print("Opening a position")
     opentrade = con.open_trade(symbol=d, is_buy=is_buy,
                             amount=amount, time_in_force='GTC',
                             order_type='AtMarket', is_in_pips=True,
@@ -37,30 +37,50 @@ def getLastRsiValue(devise):
     return rsiList[-1];
 
 
+def checkIfClosedPosition():
+    #ind indicates whether the symbol has still an open position or not. 1 for yes and 0 for no.
+    ind = 0
+    #retrieve open positions
+    openpositions = con.get_open_positions(kind='list')
+    for symbol in openSymbols:
+        for position in openpositions:
+            if (position['currency'] == symbol):
+                ind = 1
+        #symbol does not have a current open position
+        if (ind == 0):
+            openSymbols.remove(symbol)
+            symbols.append(symbol)
+        #reste indicator
+        else:
+            ind = 0
+
+
+
 con = fxcmpy.fxcmpy(access_token=token, log_level='error', server='demo', log_file='log.txt')
 
 while (True):
-    for i in range(len(symbol)):
-        rsiValue = getLastRsiValue(symbol[i])
+    print(openSymbols)
+    print(symbols)
+    checkIfClosedPosition()
+    for symbol in symbols:
+        rsiValue = getLastRsiValue(symbol)
 
-        print(symbol[i] + " RSI : " + str(rsiValue))
+        print(symbol + " RSI : " + str(rsiValue))
 
         if (rsiValue > upper_rsi):
-            enter(False, symbol[i]) # Sell
-            symbolEnter.append(symbol[i])
-            symbol.remove(symbol[i])
-            con.close()
-            exit(1)
+            position(False, symbol) # Sell
+            openSymbols.append(symbol)
+            symbols.remove(symbol)
+            #con.close()
+            #exit(1)
         if (rsiValue < lower_rsi):
-            enter(True, symbol[i]) # Buy
-            symbolEnter.append(symbol[i])
-            symbol.remove(symbol[i])
-            con.close()
-            exit(1)
+            position(True, symbol) # Buy
+            openSymbols.append(symbol)
+            symbols.remove(symbol)
+            #con.close()
+            #exit(1)
     time.sleep(5)
     print("----------------");
-
-
 
 con.close()
 
@@ -72,7 +92,7 @@ def exit(BuySell=None):
     if BuySell == "S":
         isbuy = False
     for position in openpositions:
-        if position['currency'] == symbol:
+        if position['currency'] == symbols:
             if BuySell is None or position['isBuy'] == isbuy:
                 print("   Closing tradeID: " + position['tradeId'])
                 try:
