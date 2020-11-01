720bc5c59fc964b43e59ea4233e74e43d53bd1da
Philippe Huron <36928809+philippehuron@users.noreply.github.com>
2020-11-01 11:29:06 +0100

    Update Bot.py

--- version_1/Bot.py
+++ version_1/Bot.py
@@ -10,14 +10,10 @@ from pyti.exponential_moving_average import exponential_moving_average as ema
 
 ###### USER PARAMETERS ######
 #phiphi
-#token = "1a7dd8a06ab9e998d650cb750f2bd039860af8c2"
+token = "1a7dd8a06ab9e998d650cb750f2bd039860af8c2"
 #moi
-token = '52f024b13ae656b23cdbf7af83b784fece1827f8'
+#token = '52f024b13ae656b23cdbf7af83b784fece1827f8'
 symbols = ['EUR/USD', 'GBP/USD', 'AUD/USD']
-timeframe = "m1"
-rsi_periods = 14
-upper_rsi = 70.0
-lower_rsi = 30.0
 bTValues = []
 buyposition = 0
 sellposition = 0
@@ -28,11 +24,6 @@ openSymbols = []
 
 def position(is_buy, d, amount, limit, stop):
     print("Opening a position")
-    print(amount)
-    print(limit)
-    print(stop)
-    print(d)
-    print(is_buy)
     opentrade = con.open_trade(symbol=d, is_buy=is_buy,
                             amount=amount, time_in_force='GTC',
                             order_type='AtMarket', is_in_pips=False,
@@ -84,13 +75,12 @@ def backTesting():
         bTValues = con.get_candles(symbol, period="H1", start=sDate, end=eDate, columns=['date', 'bidclose', 'askclose'], with_index=False)
 
         while (ind < len(bTValues)):
-            #STRATEGY TO TEST
-            rsiData = con.get_candles(symbol, period="H1", end=bTValues['date'][ind], number=500)
+            #retrieving candles
             smaData = con.get_candles(symbol, period="H1", end=bTValues['date'][ind], number=110)
             slowemaData = con.get_candles(symbol, period="H1", end=bTValues['date'][ind], number=26)
             fastemaData = con.get_candles(symbol, period="H1", end=bTValues['date'][ind], number=9)
 
-            rsiList = rsi(rsiData['bidclose'], 14)
+            #calculating indicators
             smaList = sma(smaData['bidclose'], 110)
             slowemaList = ema(slowemaData['bidclose'], 26)
             fastemaList = ema(fastemaData['bidclose'], 9)
@@ -99,13 +89,16 @@ def backTesting():
                 stockfastEma = fastemaList[-1]
                 stockslowEma = slowemaList[-1]
 
+            #checking if indicators cross (trends)
             emaInd = checkemaCross(emaInd, slowemaList, fastemaList, stockslowEma, stockfastEma)
 
+            #long conditions
             if (buyposition == 0 and emaInd == 1 and bTValues['bidclose'][ind] > smaList[-1]):
                 print("You opened a buy position on " + str(bTValues['date'][ind]))
                 print()
                 buyposition = bTValues['bidclose'][ind]
 
+            #short conditions
             if (sellposition == 0 and bTValues['bidclose'][ind] < smaList[-1] and emaInd == 0):
                 print("You opened a sell position on " + str(bTValues['date'][ind]))
                 print()
@@ -114,27 +107,28 @@ def backTesting():
             stockfastEma = fastemaList[-1]
             stockslowEma = slowemaList[-1]
 
+            #profit or loss conditions + balance update
             if (buyposition != 0 and bTValues['bidclose'][ind] < buyposition - (buyposition/10000) * 70):
                 buyposition = 0
-                balance = balance - ((balance/10000) * 70)
+                balance = balance - (((balance/10000) * 70))
                 print("You lost money!")
                 print("New balance : " + str(balance))
                 print()
             if (buyposition != 0 and bTValues['bidclose'][ind] >= buyposition + (buyposition/100)*16/10):
                 buyposition = 0
-                balance = balance + ((balance/100)*16/10)
+                balance = balance + (((balance/100)*16/10))
                 print("You won money!")
                 print("New balance : " + str(balance))
                 print()
             if (sellposition != 0 and bTValues['bidclose'][ind] > sellposition + (sellposition/10000) * 70):
                 sellposition = 0
-                balance = balance - ((balance/10000) * 70)
+                balance = balance - (((balance/10000) * 70))
                 print("You lost money!")
                 print("New balance : " + str(balance))
                 print()
             if (sellposition != 0 and bTValues['bidclose'][ind] < sellposition - (sellposition/100)*16/10):
                 sellposition = 0
-                balance = balance + ((balance/100)*16/10)
+                balance = balance + (((balance/100)*16/10))
                 print("You won money!")
                 print("New balance : " + str(balance))
                 print()
@@ -153,13 +147,13 @@ def liveApp():
     
     while (True):
         pos = checkIfClosedPosition()
+        #retrieving candles
         liveValue = con.get_candles(symbol, period="H1", columns=['date', 'bidclose', 'askclose'], with_index=False, number=1)
-        rsiData = con.get_candles(symbol, period="H1", number=500)
         smaData = con.get_candles(symbol, period="H1", number=110)
         slowemaData = con.get_candles(symbol, period="H1", number=26)
         fastemaData = con.get_candles(symbol, period="H1", number=9)
 
-        rsiList = rsi(rsiData['bidclose'], 14)
+        #calculating indicators
         smaList = sma(smaData['bidclose'], 110)
         slowemaList = ema(slowemaData['bidclose'], 26)
         fastemaList = ema(fastemaData['bidclose'], 9)
@@ -168,23 +162,33 @@ def liveApp():
             stockfastEma = fastemaList[-1]
             stockslowEma = slowemaList[-1]
 
+        #checking if indicators cross (trends)
         emaInd = checkemaCross(emaInd, slowemaList, fastemaList, stockslowEma, stockfastEma)
 
         if (pos == 0 and emaInd == 1 and liveValue['bidclose'][0] > smaList[-1]):
-            limit = liveValue['bidclose'][0] + (liveValue['bidclose'][0]/100)*16/10
-            stop = liveValue['bidclose'][0] - (liveValue['bidclose'][0]/10000) * 70
-            position(True, symbol, 500, limit, stop) # Buy
+            #Calculation of our profit gain / stop loss percentage based on live value
+            limit = liveValue['bidclose'][0] + (liveValue['bidclose'][0]/100)*16/10/2
+            stop = liveValue['bidclose'][0] - (liveValue['bidclose'][0]/10000)*70/2
+            #Buy Position
+            position(True, symbol, 1000, limit, stop)
         if (pos == 0 and liveValue['bidclose'][0] < smaList[-1] and emaInd == 0):
-            limit = liveValue['bidclose'][0] - (liveValue['bidclose'][0]/100)*16/10
-            stop = liveValue['bidclose'][0] + (liveValue['bidclose'][0]/10000) * 70
-            position(False, symbol, 500, limit, stop) # Sell
-        #time.sleep(5)
+            #Calculation of our profit gain / stop loss percentage based on live value
+            limit = liveValue['bidclose'][0] - (liveValue['bidclose'][0]/100)*16/10/2
+            stop = liveValue['bidclose'][0] + (liveValue['bidclose'][0]/10000)*70/2
+            # Sell position 
+            position(False, symbol, 1000, limit, stop)
 
+<<<<<<< Updated upstream:version_1/Bot.py
 con = fxcmpy.fxcmpy(access_token=token, log_level='error', server='demo', log_file='../log.txt')
+=======
+#Connection to fxcm api
+con = fxcmpy.fxcmpy(access_token=token, log_level='error', server='demo', log_file='log.txt')
+>>>>>>> Stashed changes:Bot.py
 
 if (len(sys.argv) > 1 and sys.argv[1].lower() == '-b'):
     backTesting()
 else:
     liveApp()
 
+#closing connection to fxcm api
 con.close()
